538b5a96a1a9da7d831a1c510df3ae79bdfc3db3
zFrozead0s <141191707+zFrozead0s@users.noreply.github.com>
2024-07-20 16:18:39 +0200

    Update InventoryClickListener.php

--- src/LFFPlugin/event/InventoryClickListener.php
+++ src/LFFPlugin/event/InventoryClickListener.php
@@ -1,1 +1,36 @@
+<?php
 
+namespace LFFPlugin\event;
+
+use pocketmine\event\Listener;
+use pocketmine\event\inventory\InventoryTransactionEvent;
+use pocketmine\player\Player;
+use LFFPlugin\Main;
+
+class InventoryClickListener implements Listener {
+
+    /** @var Main */
+    private $plugin;
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
+    }
+
+    public function onInventoryTransaction(InventoryTransactionEvent $event): void {
+        $transaction = $event->getTransaction();
+        $player = $transaction->getSource();
+
+        if ($player instanceof Player) {
+            foreach ($transaction->getActions() as $action) {
+                $item = $action->getSourceItem();
+                if ($item->getCustomName() !== "") {
+                    // Mensaje a mostrar en el chat
+                    $message = "El usuario: " . $player->getName() . " de clase: " . $item->getCustomName() . " busca una facción";
+                    $this->plugin->getServer()->broadcastMessage($message);
+                    $event->cancel(); // Evita que el jugador tome el item del inventario
+                    return;
+                }
+            }
+        }
+    }
+}

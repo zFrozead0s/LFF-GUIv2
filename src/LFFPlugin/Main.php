3e477a240bc3f0f33da103326c7514b6f65e98a0
zFrozead0s <141191707+zFrozead0s@users.noreply.github.com>
2024-07-20 16:03:28 +0200

    Actualizar Main.php

--- src/LFFPlugin/Main.php
+++ src/LFFPlugin/Main.php
@@ -3,11 +3,8 @@
 namespace LFFPlugin;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
 use pocketmine\utils\Config;
-use pocketmine\player\Player;
-use LFFPlugin\Command\LFFCommand;
+use LFFPlugin\command\LFFCommand;
 
 class Main extends PluginBase {
 
@@ -19,8 +16,7 @@ class Main extends PluginBase {
         $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
 
         $this->getServer()->getCommandMap()->register("lff", new LFFCommand($this));
-        $this->getLogger()->info("LFFPlugin ha sido habilitado.
-        - Hecho por zFrozead0s para servidores HCF - Factions");
+        $this->getLogger()->info("LFFPlugin ha sido habilitado.");
     }
 
     public function getConfigData(): Config {

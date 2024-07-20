df2e352dffc71ce56ee7cd87b93a48d4ba42bf43
zFrozead0s <141191707+zFrozead0s@users.noreply.github.com>
2024-07-20 15:53:24 +0200

    Update LFFCommand.php

--- src/LFFPlugin/Command/LFFCommand.php
+++ src/LFFPlugin/Command/LFFCommand.php
@@ -1,1 +1,51 @@
+<?php
 
+namespace LFFPlugin\Command;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\item\Item;
+use pocketmine\inventory\Inventory;
+use pocketmine\inventory\SimpleInventory;
+use pocketmine\plugin\PluginBase;
+use LFFPlugin\Main;
+
+class LFFCommand extends Command {
+
+    /** @var Main */
+    private $plugin;
+
+    public function __construct(Main $plugin) {
+        parent::__construct("lff", "Abre el menú Looking for Faction.", "/lff", []);
+        $this->setPermission("lffplugin.command.lff");
+        $this->plugin = $plugin;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if (!$this->testPermission($sender)) {
+            return false;
+        }
+
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("Este comando solo puede ser usado en el juego.");
+            return false;
+        }
+
+        $config = $this->plugin->getConfigData();
+        $menuTitle = $config->get("menu-title", "Looking for Faction");
+        $options = $config->get("options", []);
+
+        $inventory = new SimpleInventory(count($options));
+
+        foreach ($options as $index => $option) {
+            $item = Item::get(Item::PAPER);
+            $item->setCustomName($option["name"]);
+            $item->setLore([$option["description"]]);
+            $inventory->setItem($index, $item);
+        }
+
+        $sender->addWindow($inventory);
+        return true;
+    }
+}
